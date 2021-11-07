4d046691727e558c600e57016722df7e9bd7378f
SoSoJigsaw <felipesobral_@hotmail.com>
2021-11-06 23:49:12 -0300

    Atualizando DataFrames

--- Data Cleaning/vacinacao_estatisticas.py
+++ Data Cleaning/vacinacao_estatisticas.py
@@ -59,7 +59,6 @@ df = df.sort_values(['Dado'], ascending=True)
 
 df.info(verbose=False, memory_usage="deep")
 print('\n', df.dtypes, '\n')
-print(df)
 
 # Exportando o Dataframe tratado em arquivo csv
 df.to_csv("/home/sobral/Carcara/Aplicação Web/app/data/vacinacao-estatisticas.csv", index=False)
